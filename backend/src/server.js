1a7542a191fb1af61bd8fbb14918bf9f662aca7e
RaadC <jannradley.maniago@gsfe.tupcavite.edu.ph>
2026-02-27 11:12:59 +0800

    for dp

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import connectDB from "./config/db.js";
 import departmentRoutes from "./routes/departmentRoutes.js";
@@ -15,8 +16,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
+
 app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use("/api/departments", departmentRoutes);
 app.use("/api/items", itemRoutes);
@@ -26,7 +33,12 @@ app.use("/api/suggest", suggestRoutes);
 app.use("/api/signatory", signatoryRoutes);
 app.use("/api/admin", adminRoutes);
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 const startServer = async () => {
   try {
